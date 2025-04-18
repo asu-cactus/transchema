45c80e7aebece6b2b021c570d058b87acd594871
jrt1899 <jaytandel1899@gmail.com>
2025-04-18 21:55:02 +0000

    Language Implementation first draft

--- hint.py
+++ hint.py
@@ -4,6 +4,7 @@ import summary
 from model.aggregation.pwr import load_trained_model, predict_columns
 from model.join.pwr import predict_join_columns
 from sklearn.preprocessing import LabelEncoder
+import hint_v3 as h3
 
 def get_hints(prompt_type, hint_source,target_data_schema, file_count,source_data_name_list,source_data_schema_list,directory,len_idx_target_idx, hint_flag, hints_truncate) :
     hints = []
@@ -44,7 +45,20 @@ def get_hints(prompt_type, hint_source,target_data_schema, file_count,source_dat
         source_dfs = [tables[table] for table in tables if table != target_table_name]
         hints = generate_transformation_hints(tables,source_dfs, target_df, mode='light', candidate_matching_columns=candidate_matching_columns, candidate_key_columns=key_candidates, type=prompt_type)
         # print(hints)
+
+    elif(hint_source == "v3") :
+        if(prompt_type == "join") :
+            hints = h3.get_join_hints(hint_source,file_count,source_data_name_list,source_data_schema_list,directory,len_idx_target_idx, hint_flag, hints_truncate)
+        if(prompt_type == "group_by_aggregate") :
+            hints = h3.get_groupby_aggregate_hints(hint_source,file_count,source_data_name_list,source_data_schema_list,directory,len_idx_target_idx, hint_flag, hints_truncate)
+        if(prompt_type == "union") : 
+            hints = h3.get_union_hints(hint_source,file_count,source_data_name_list,source_data_schema_list,directory,len_idx_target_idx, hint_flag, hints_truncate)
+        if(prompt_type == "get_next_operator") :
+            table_matching = h.get_table_matching(source_data_schema_list, source_data_name_list, target_data_schema)
+            # column_table_mapping = h.get_column_table_mapping(target_data_schema, source_data_schema_list, source_data_name_list)
+            hints = [table_matching]
         
+        # return [hints]
     else : 
         pass
     return hints 
