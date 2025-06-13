07a68bc2ba101f50f4e78a7119d8475e274a1c42
jrt1899 <jaytandel1899@gmail.com>
2025-06-13 10:15:54 -0700

    Readme Changes

--- experiment_multistep.sh
+++ experiment_multistep.sh
@@ -1,10 +1,10 @@
 export PYTHONPATH=$(pwd)
 
 python3 critique_data.py \
-  --len-id 3 \
-  --max-len-id 6 \
-  --target-id 6 \
-  --max-target-id 20 \
+  --len_id 3 \
+  --max_len_id 6 \
+  --target_id 6 \
+  --max_target_id 20 \
   --target-per 25 \
   --no-perc \
   --hint-source v3 \
@@ -17,13 +17,14 @@ python3 critique_data.py \
   --join-hints-truncate 0.027387593197926163 0.8763891522960383 0.6923226156693141 0.8946066635038473 0.14038693859523377 0.8007445686755367 \
   --aggregate-hints-truncate 0.9 0.1 0.9 0.1 0.9 0.1 0.9 0.1 0.9 0.1 \
   --critique_setting metadata \
+  --critique_type soft \
   --token-limit 120000 \
   --model gpt-4.1-mini \
   --log-dir logs \
   --experiment-name feature_v3_4_bad \
-  --no-of-runs 1 \
+  --no_of_runs 1 \
   --hints-v3-truncates '{"t1":0.3,"t2":0.3,"t3":0.3,"t4":10,"t5":0.1,"t6":0.4,"t7":0.8,"t8":0.7,"t9":0.3,"t10":0.5,"t11":0.3,"t12":0.3,"t13":0.2}' \
-  --intermediate-materialization-flag 0 \
-  --use-old-prompt 0 \
-  --combine-ask-and-configure 0 \
-  --no-thinking 0 \
+  --intermediate_materialization
+  #--use_old_prompt 
+  --combine_ask_and_configure \
+  --no_thinking \
